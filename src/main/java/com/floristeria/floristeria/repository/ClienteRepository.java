5b38bad8b8a76b03dff61ecb3fc020fe5f3521da
unknown <jrodmez97@gmail.com>
2026-06-12 01:33:09 -0500

    feat: agregar repositorios ClienteRepository y DireccionRepository
    - ClienteRepository: findByEmail, existsByEmail
    - DireccionRepository: findByClienteId
    - Ambos extienden JpaRepository con soft delete heredado

---
+++ src/main/java/com/floristeria/floristeria/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.floristeria.floristeria.repository;
+
+import com.floristeria.floristeria.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    Optional<Cliente> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
