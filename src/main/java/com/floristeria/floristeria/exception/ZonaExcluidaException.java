7f9fe644cbc15e53170be4bf21da9dd20ccd55f3
unknown <jrodmez97@gmail.com>
2026-07-13 03:49:34 -0500

    feat: agregar ZonaExcluidaException para zonas excluidas

---
+++ src/main/java/com/floristeria/floristeria/exception/ZonaExcluidaException.java
@@ -0,0 +1,7 @@
+package com.floristeria.floristeria.exception;
+
+public class ZonaExcluidaException extends RuntimeException {
+    public ZonaExcluidaException(String message) {
+        super(message);
+    }
+}
